323c746f2d50fb504ba48001b14893f094f58d34
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2023-01-07 03:46:53 -0800

    Add files via upload

---
+++ class 05/app.js
@@ -0,0 +1,162 @@
+
+
+// var character = prompt("Enter a Character")
+
+
+
+// var asciicode = character.charCodeAt(0);
+
+
+// if(asciicode >= 65 && asciicode <= 90){
+//    console.log("UpperCase");
+// }
+// else if(asciicode >= 97 && asciicode <= 122){
+//     console.log("LowerCase");
+// }
+
+// console.log(asciicode);
+
+// var name = "muzammil"
+
+
+
+// Nested If Else
+
+
+// var a = 10 ;
+// var b = 20 ;
+
+
+// if(a == 10){
+// if(b == 20){
+
+//     console.log("a = 10");
+// }else{
+//     console.log("false");
+// }
+// }
+// else{
+//     console.log("a not = 10");
+// }
+
+
+
+// Array
+
+// var names = [ "ali" , "ahmed" , "muzammil"] ;
+
+// console.log(names[2]);
+
+
+
+
+// var cities = [ "karachi" , "lahore" , "hyderabad" , "islamabad"] ;
+
+// console.log( cities[3] );
+
+
+
+// var multivalues = ["String" , 80 , undefined , null , true] ;
+
+
+// var bool = multivalues[4] ;
+
+// console.log(bool);
+
+
+// console.log( multivalues.length );
+
+
+
+
+
+
+// Methods of Array
+
+// var birds = [] ;
+
+// birds[0] = "pigeon"
+// birds[1] = "sparrow"
+
+// console.log(birds);
+
+
+// var cities = ["karachi" , "lahore" , "multan" , "islamabad" , "rawalpindi"]
+
+// cities.pop() ;
+// cities.pop() ;
+
+// console.log(cities);
+
+// cities.push("hyderabad" , "quetta")
+
+// console.log(cities);
+
+
+
+// var cities = ["karachi" , "lahore" , "multan" , "islamabad" , "rawalpindi"]
+
+
+// cities.shift() ;
+// cities.shift() ;
+
+// console.log( cities );
+
+// cities.unshift("hyderabad" , "quetta");
+
+// console.log(cities);
+
+
+
+// var cities = ["karachi" , "lahore" , "multan" , "islamabad" , "rawalpindi"]
+
+// cities.splice(2 , 3) ;
+
+// console.log(cities);
+
+
+// cities.splice(2 , 1 , "hyderabad")
+
+// console.log(cities);
+
+
+
+
+
+
+// var cities = ["karachi" , "lahore" , "multan" , "islamabad" , "rawalpindi"] ;
+
+
+// var copy = cities.slice(1 , 4 ) ;
+
+// console.log(copy);
+
+
+
+// splice( index no , removing length , adding elements )
+
+// slice(index no , copy length no from begining)
+
+
+
+
+
+
+
+// Multidimensional Array 
+
+// var arr = [ ["ali" , "bilal" ] , ["ahmed"] ] ;
+
+// console.log( arr[0][1] );
+
+
+
+
+
+
+
+
+
+
+
+

6d1b9e168cb4eb9ff7dd3c26ebe3799b7be1a756
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2023-01-07 21:54:40 -0800

    Add files via upload

---
+++ class 06/app.js
@@ -0,0 +1,146 @@
+// alert("hello world")
+
+
+
+
+//for Loop
+
+// for( var i = 0; i <= 10 ; i++ ){
+//     document.write("javascript" + "<br>");
+// }
+
+
+// var i;
+// for(i = 0 ; i <= 15 ; i++){
+//     document.write("Hello World" + "<br>")
+// }
+
+
+
+
+
+
+// var cities = [ "karachi" , "lahore" , "multan" , "quetta" , "peshawar" , "hyderabad"] ;
+
+// console.log(cities[0]);
+// console.log(cities[1]);
+// console.log(cities[2]);
+// console.log(cities[3]);
+// console.log(cities[4]);
+
+// for(var i = 0 ; i < cities.length ; i++){
+//  console.log(cities[i]);
+// }
+
+
+
+// var tableNumber = 8 ;
+
+// for(var i = 1 ; i <= 10 ; i++){
+//     console.log( tableNumber + " X " + i + " = " + tableNumber*i );
+// }
+
+
+
+
+
+// var cities = [ "karachi" , "lahore" , "multan" , "quetta" , "peshawar" , "hyderabad"] ;
+
+
+// var cityName = prompt("Enter City Name") ;
+
+// var flag = false ;
+
+// for(var i = 0 ; i < cities.length ; i++){
+//     if(cities[i] == cityName){
+//         console.log("Correct");
+//         flag = true
+//     }
+// }
+
+// if(flag == false){
+//     console.log("Incorrect");
+// }
+
+
+
+// for(var i = 0 ; i <= 10 ; i++){
+//     if(i < 5){
+//         console.log("I am True");
+//     }
+//     else{
+//         break ;
+//     }
+//     console.log("Working");
+// }
+
+
+
+
+// Changing Case
+
+
+
+// var name1 = "muzammil" ;
+
+// console.log( name1.toUpperCase() );
+
+// var name2 = "MUZAMMIL" ;
+
+// console.log( name2.toLowerCase() );
+
+
+
+
+// String Methods
+
+// var str = "Pakistan" ;
+
+// console.log( str.length );
+
+
+
+// console.log( str.indexOf("t") );
+// console.log( str.indexOf("a") );
+
+// console.log( str.lastIndexOf('a') );
+
+
+// console.log( str.charAt(6) );
+
+
+
+
+
+
+// Math Methods
+
+
+// var num = 10.84646 ;
+
+
+// var method1 = Math.round(num) ;
+
+// var method2 = Math.ceil(num) ;
+
+// var method3 = Math.floor(num) ;
+
+// console.log(method3);
+
+
+
+
+var val = Math.floor(Math.random() * 100) + 1 ;
+
+
+console.log(val);
+
+
+
+
+
+
+
+
+
+

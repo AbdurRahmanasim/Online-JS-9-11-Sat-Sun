173901dec80eb27ddd178b2caf0ccf5b4245c91e
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2023-01-13 23:04:08 -0800

    Add files via upload

---
+++ class 07/app.js
@@ -0,0 +1,149 @@
+// console.log("JavaScript");
+
+
+
+
+// String Methods
+
+// var name = prompt("Enter Name") ;
+
+
+// console.log(name);
+// console.log(name.trim());
+
+
+// Number Methods 
+
+
+// var num1 = "99" ;
+
+// var result1 = Number(num1)
+
+// var result2 = parseInt(num1) ;
+
+// console.log( result1 + 1 );
+
+// console.log( result2 + 5 );
+
+
+
+
+// Fixed 
+
+
+// var num = 9.75353675 ;
+
+// var result = num.toFixed(2)
+
+// console.log(num);
+// console.log(result);
+
+
+
+
+// Date 
+
+
+// var  date = new Date() ;
+
+// console.log(date);
+
+
+
+
+// Date Methods 
+
+// var now = new Date() ;
+
+// console.log(  now.toDateString()  );
+
+
+// console.log(  now.getDate()  );
+
+
+// console.log(  now.getFullYear()  );
+
+// console.log(  now.getMonth()  );
+
+
+// console.log(  now.getDay()  );
+
+
+// var str = "javascript" ;
+
+// console.log(str);
+
+
+
+// var now = new Date('January 15 2013') ;
+
+// console.log(now);
+
+
+// time related methods 
+
+
+// var now = new Date() ;
+
+
+// console.log(  now.getHours()  ); 
+
+// console.log(  now.getMinutes()  );
+
+// console.log(  now.getSeconds()  );
+
+// console.log(  now.getMilliseconds()  );
+
+
+
+
+
+
+// var now = new Date() ;
+
+
+// console.log(  now.getTime()   );
+
+// 1st January 1970  MilliSeconds
+
+
+
+// Set Methods in Date 
+
+
+// var currentDate = new Date() ;
+
+// console.log( currentDate.setDate(15) );
+
+
+// console.log( currentDate.setFullYear(2023)  );
+// console.log( currentDate.setFullYear('2023')  );
+
+
+
+
+
+// Function 
+
+
+// console.log("Hello World");
+
+
+
+
+
+
+// function call(){
+//     console.log("Hello World");
+// }
+
+
+// call()
+// call()
+// call()
+// call()
+
+
+
+
+

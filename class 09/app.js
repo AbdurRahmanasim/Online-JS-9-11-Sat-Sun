e66927a7e0a6cea96bc0444ea3b6f7d33a3d62e5
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2023-01-22 22:28:15 -0800

    Add files via upload

---
+++ class 09/app.js
@@ -0,0 +1,72 @@
+// var color = "";
+
+// switch (color) {
+//   case "red":
+//     alert("stop");
+//     break;
+
+//   case "yellow":
+//     alert("ready to go");
+//     break;
+
+//   case "green" :
+//     alert("Go")
+//     break;
+
+//    default :
+//     alert("invalid color")
+// }
+
+
+
+
+
+// For Loop 
+
+// for(var i = 0 ; i <10 ; i++){
+//     alert(i);
+// }
+
+
+// While loop 
+
+
+// var i = 0
+// while(i < 0){
+//     console.log("Hello World");
+//      i++
+// }
+
+
+// Do While Loop 
+
+// var i = 0 ;
+// do{
+//     console.log("do while loop");
+//     i++
+// }
+// while(i < 0)
+
+
+
+
+// Events 
+
+
+
+
+
+
+function print(){
+    console.log("Printed");
+}
+
+
+
+
+
+
+
+
+
+

094012b7aeaa187f82892fdfcf167aea34df2d0b
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2023-01-27 22:10:43 -0800

    Add files via upload

---
+++ class 10/app.js
@@ -0,0 +1,61 @@
+
+
+// function print(){
+//     console.log("Event");
+// }
+
+
+// function focusent(){
+//     console.log("Focus");
+// }
+// function blurent(){
+//     console.log("Blur");
+// }
+
+
+// var heading = document.getElementById("heading") ;
+
+// console.log(heading);
+
+
+
+
+
+// function getValue(){
+
+// var val = document.getElementById("val").value ;
+
+// console.log(val);
+
+// }
+
+// function showtxt(){
+//     var val = document.getElementById("para").innerHTML
+
+//     console.log(val);
+// }
+
+
+// function change(){
+//     var img = document.getElementById("image") ;
+//     img.src = "car1.jpg" ;
+// }
+
+// function on(){
+//     var bulb = document.getElementById("bulb")
+//     bulb.src = "pic_bulbon.gif"
+// }
+
+// function off(){
+//     var bulb = document.getElementById("bulb")
+//     bulb.src = "pic_bulboff.gif"
+// }
+
+
+// var lang = document.getElementsByClassName("lang") ;
+
+// console.log(lang[2].innerHTML);
+
+// var getVal = document.getElementsByTagName('p') ;
+
+// console.log(getVal[2].innerHTML);
